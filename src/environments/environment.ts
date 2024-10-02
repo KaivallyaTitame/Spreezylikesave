e3f843e2b317310fc83d9ac651879e10b761ab79
Nikhil Gaikwad <gaikwadnikhil10012003@gmail.com>
2024-10-02 13:16:46 +0530

    small change in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiGateway: "http://192.168.1.105:8081",
+  apiGateway: " http://localhost:8081",
   firebase: {
     apiKey: "",
     authDomain: "",
