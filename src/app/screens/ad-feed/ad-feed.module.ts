e00ed155d5545829ec1c92b1932c9cfd28a0123c
anushkanaik31 <141602577+anushkanaik31@users.noreply.github.com>
2024-10-07 10:16:54 +0530

    Renamed Home module to AdFeed module

--- src/app/screens/ad-feed/ad-feed.module.ts
+++ src/app/screens/ad-feed/ad-feed.module.ts
@@ -1,36 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+<<<<<<< HEAD:src/app/screens/home-screen/home-screen.module.ts
+import { HomeRoutingModule } from './home-screen-routing.module';
+import { HomeScreenComponent } from './home-screen.component';
+import { BusinessBottomNavbarComponent } from '../business-home/business-bottom-navbar/business-bottom-navbar.component';
+import { BusinessTopNavbarComponent } from '../business-home/business-top-navbar/business-top-navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+=======
 import { AdFeedRoutingModule } from './ad-feed-routing.module';
 import { AdFeedComponent } from './ad-feed.component';
-
-// import { OfferDescriptionComponent } from './offer-description/offer-description.component';
-import { EventComponent } from './Event/Event.component';
-import { PostComponent } from './Post/Post.component';
-import { CouponComponent } from './Coupon/Coupon.component';
-
-
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SharedModule } from 'src/app/shared/shared-module/shared-module.module';
+>>>>>>> 6a22698 (Renamed Home module to AdFeed module):src/app/screens/ad-feed/ad-feed.module.ts
+import { BusinessHomeModule } from '../business-home/business-home.module';
+import { ConsumerHomeModule } from '../consumer-home/consumer-home.module';
 
 
 
 @NgModule({
   declarations: [
     AdFeedComponent,
-    // OfferDescriptionComponent,
-    EventComponent,
-    PostComponent,
-    CouponComponent
-
-    
-  
-   
   ],
   imports: [
     CommonModule,
     AdFeedRoutingModule,
-    FontAwesomeModule,
-    SharedModule
-  ]
+    BusinessHomeModule,
+    ConsumerHomeModule
+  ],
+  exports: [
+    AdFeedComponent,
+  ],
 })
 export class AdFeedModule { }
