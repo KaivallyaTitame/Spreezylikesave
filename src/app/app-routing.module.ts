1d6fc03182087e2115a602a511fa2d25e17b5652
vaibhav <indurevaibhav9@gmail.com>
2024-10-09 04:05:19 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -49,7 +49,7 @@ const routes: Routes = [
   {
     path: "otpscreen/:mobileNumber",
     loadChildren: () =>
-      import("./screens/otpscreen/otpscreen.module").then(
+      import("./screens/otpScreen/otpscreen.module").then(
         (module) => module.OtpscreenModule
       ),
 <<<<<<< HEAD
