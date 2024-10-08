116aea2571bd8c563f39fa26f740a0b52fcccdd0
vaibhav <indurevaibhav9@gmail.com>
2024-10-09 04:05:19 +0530

    updated

--- src/app/screens/otpScreen/otpscreen.module.ts
+++ src/app/screens/otpScreen/otpscreen.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { OtpScreenRoutingModule } from './otpscreen-routing.module';
+import { OtpscreenRoutingModule } from './otpscreen-routing.module';
 import { OtpscreenComponent } from './otpscreen.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared-module/shared-module.module';
@@ -15,9 +15,9 @@ import { LoaderComponent } from 'src/app/components/loader/loader.component';
     ],
   imports: [
     CommonModule,
-    OtpScreenRoutingModule,
+    OtpscreenRoutingModule,
     SharedModule,
     ReactiveFormsModule
   ]
 })
-export class OtpScreenModule { }
+export class OtpscreenModule { }
