f0067327c9e99d076cb354a695b34f08e18ccc1f
anushkanaik31 <141602577+anushkanaik31@users.noreply.github.com>
2024-10-17 10:47:12 +0530

    Consumer and Business data is displayed through dummy data

--- src/app/screens/consumer-home/consumer-top-navbar/consumer-top-navbar.component.ts
+++ src/app/screens/consumer-home/consumer-top-navbar/consumer-top-navbar.component.ts
@@ -1,40 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { faArrowRightFromBracket, faBars, faCircleQuestion, faFileLines, faFilePen, faGear, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { ConsumerNavigationService } from 'src/app/services/consumer-navigation.service';
-import { UserProfileDto } from 'src/app/models/UserProfileDTO';
+import { UserProfileDTO } from 'src/app/models/UserProfileDTO';
 
 @Component({
   selector: 'app-consumer-top-navbar',
   templateUrl: './consumer-top-navbar.component.html',
   styleUrls: ['./consumer-top-navbar.component.css']
 })
-export class ConsumerTopNavbarComponent {
-<<<<<<< HEAD
-  faBars = faBars;
-  faSearch = faSearch;
-  faMessage = faMessage;
-  faGear = faGear;
-  faFilePen = faFilePen;
-  faDiceD20 = faDiceD20;
-  faCircleQuestion = faCircleQuestion;
-  faFileLines = faFileLines;
-  faArrowRightFromBracket = faArrowRightFromBracket;
-<<<<<<< HEAD
+export class ConsumerTopNavbarComponent implements OnInit {
+  faIcons = {
+    bars: faBars,
+    search: faSearch,
+    gear: faGear,
+    filePen: faFilePen,
+    fileLines: faFileLines,
+    circleQuestion: faCircleQuestion,
+    arrowRightFromBracket: faArrowRightFromBracket
+  };
 
-  toggleSidebar() {
-    const consumerTopNavbar = document.querySelector('.consumer-top-navbar') as HTMLElement;
-    const bottomNavbar = document.querySelector('#bottom-navbar') as HTMLElement;
+  consumer: any;  
 
-    if (consumerTopNavbar) {
-      consumerTopNavbar.classList.toggle('drawer-open'); // Change this to the appropriate class for showing/hiding the top navbar
-    }
+  constructor(private router: Router, private consumernavigationservice: ConsumerNavigationService) {}
 
-    if (bottomNavbar) {
-      bottomNavbar.classList.toggle('bottom-navbar-shifted');
-    }
+  ngOnInit(): void {
+    this.fetchConsumerInformation();
   }
-<<<<<<< HEAD
 
   fetchConsumerInformation() {
     this.consumernavigationservice.getConsumerDetails().subscribe({
@@ -50,39 +42,4 @@ export class ConsumerTopNavbarComponent {
       }
     });
   }
-=======
->>>>>>> 1c8e8a1 (improved folder and file structure, edited names of consumer and business navigation bars)
-=======
->>>>>>> ede22af (Business and consumer navigation bar working)
 }
-=======
-  faIcons = {
-    bars: faBars,
-    search: faSearch,
-    gear: faGear,
-    filePen: faFilePen,
-    fileLines: faFileLines,
-    circleQuestion: faCircleQuestion,
-    arrowRightFromBracket: faArrowRightFromBracket,
-  };
-
-  constructor(private router: Router, private consumernavigationservice: ConsumerNavigationService ) {}
-
-  @Input() userinformation!: UserProfileDto[];
-  user: any;
-
-  ngOnInit(): void{
-    this.fetchUserInformation();
-  }
-
-  fetchUserInformation() {
-    this.consumernavigationservice.getUserDetails().subscribe(
-      (data: UserProfileDto[]) => {
-        this.user = data;
-        console.log('Consumer Details:', this.user);
-
-      }
-    );
-  }
-}
->>>>>>> 9200722 (Corrected Routes for consumer-home and business-home)
--- src/app/services/business-navigation-service.service.ts
+++ src/app/services/business-navigation-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { UserProfileDto } from '../models/UserProfileDTO';
+import { UserProfileDTO } from '../models/UserProfileDTO';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -10,14 +10,14 @@ export class BusinessNavigationServiceService {
 
   constructor() { }
 
-  Is_AdFeed:boolean = true
-  Is_Insights:boolean = false
+  Is_AdFeed:boolean = true;
+  Is_Insights:boolean = false;
   Is_Post:boolean = false;
   Is_Notification:boolean = false;
   Is_Profile:boolean = false;
 
-  getBusinessDetails(): Observable<UserProfileDto[]> { 
-    return this.http.get<UserProfileDto[]>('http://192.168.0.105:8762/user/profile/tanvi247', {
+  getBusinessDetails(): Observable<UserProfileDTO[]> { 
+    return this.http.get<UserProfileDTO[]>('https://dummyjson.com/c/98c2-5f66-47f6-9292', {
       responseType: 'json',
       headers: new HttpHeaders({
         'ngrok-skip-browser-warning': 'true',
