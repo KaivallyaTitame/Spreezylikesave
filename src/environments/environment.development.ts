629bad6c8854586cfe5185e88c7676391e003245
anushkanaik31 <141602577+anushkanaik31@users.noreply.github.com>
2024-11-28 09:24:47 +0530

    changed apiGateway Url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiGateway: "http://localhost:8083",
+  apiGateway: "http://localhost:8085",
   firebase: {
     apiKey: "AIzaSyAefHwV7r5LaXZHjcE3U0wxxBlQhA3BqwE",
     authDomain: "spreezy-auth.firebaseapp.com",
