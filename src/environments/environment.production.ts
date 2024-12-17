a68aa6421c0ca87207551e59897583cfa4f7b32f
madhav256 <maddynade7@gmail.com>
2024-12-17 08:19:45 +0530

    changed apiGateway from IP to localhost

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiGateway: "http://192.168.1.108:8762",
+  apiGateway: "http://localhost:8762",
   firebase: {
     apiKey: "",
     authDomain: "",
