8cb73666d8292453ff3ecf5bb5b22408015ddbc6
SairajRajput12 <sairajrajput6@gmail.com>
2025-04-17 10:45:45 +0530

    Added Fixed Portrait Orientation to the Application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
+// import { ScreenOrientation } from '@awesome-cordova-plugins/screen-orientation/ngx';
+import { ScreenOrientation } from '@capacitor/screen-orientation';
 import { Location } from '@angular/common';
 
 @Component({
@@ -25,6 +27,19 @@ export class AppComponent {
   onCloseError() {
     this.showError = false;
     this.location.back(); 
+  }
+
+
+  constructor(){
+    this.lockOrientation(); 
+  }
+
+  async lockOrientation() {
+    try {
+      await ScreenOrientation.lock({ orientation: 'portrait' });
+    } catch (error) {
+      console.error('Orientation lock failed:', error);
+    }
   }
 
 
