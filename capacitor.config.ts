0dacf788836a3596fe04f7cbf875939a66f5079b
Sukriti-419 <1032222419@mitwpu.edu.in>
2025-10-14 12:09:25 +0530

    added gayatri changes and solved xml error

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,9 +5,21 @@ const config: CapacitorConfig = {
   appName: 'Spreezy',
   webDir: 'dist/spreezy-frontend',
   server: {
-    androidScheme: 'http'
+    androidScheme: 'http',
   },
-
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 1500, // splash visible for 1.5s
+      launchAutoHide: true, // auto-hide after duration
+      launchFadeOutDuration: 1500, // fade out smoothly
+      backgroundColor: '#FFFFFFFF', // white background
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false, // no spinner
+      splashFullScreen: true, // full screen
+      splashImmersive: true, // hide system UI
+    },
+  },
 };
 
 export default config;
